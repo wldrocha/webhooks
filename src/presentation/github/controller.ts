9cbf3bd40d28fcd9cd641191048558578351b20e
Wladimir Rocha <wld.rocha@gmail.com>
2024-09-21 11:55:13 -0400

    chore: ♻️ create interfaces

--- src/presentation/github/controller.ts
+++ src/presentation/github/controller.ts
@@ -4,7 +4,11 @@ export class GithubController {
   constructor() {}
 
   webhookHandler = (req: Request, res: Response) => {
-   
+    const gihubEvent = req.headers['x-github-event'] ?? 'unknown'
+    
+    const payload = req.body
+
+    
     res.status(201).send('Accepeted')
   }
 }
