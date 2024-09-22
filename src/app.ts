cbef22c2a3431f6926bc349d47e213acd494751a
Wladimir Rocha <wld.rocha@gmail.com>
2024-09-21 21:34:24 -0400

    feat: ✨ security for this webhook

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import { envs } from './config'
 import { GithubController } from './presentation/github/controller'
 import { GithubService } from './presentation/service/github.service'
+import { GithubSha256Middleware } from './presentation/middlewares/github-sha256.middleware'
+
 ;(() => {
   main()
 })()
@@ -12,10 +14,8 @@ function main() {
   const githubService = new GithubService()
   const controller = new GithubController(githubService)
 
+  app.use(GithubSha256Middleware.verifySignature)
   app.post('/api/github', controller.webhookHandler)
-  app.get('/', (req, res) => {
-    res.send('Hello World')
-  })
 
   app.listen(envs.PORT, () => {
     console.log(`App runnig on port ${envs.PORT}`)
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -2,5 +2,6 @@ import 'dotenv/config'
 import { get } from 'env-var'
 
 export const envs = {
-  PORT: get('PORT').required().asPortNumber()
+  PORT: get('PORT').required().asPortNumber(),
+  SECRET_TOKEN: get('SECRET_TOKEN').required().asString()
 }
